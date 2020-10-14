8d0f55abdfa25d11fb74706cf69776ddec7412c4
madhura0110 <62782877+madhura0110@users.noreply.github.com>
2020-10-14 18:34:50 -0500

    Summary & Insights
    Introduced ASE Dependency Check Summary.

--- ase-dependency-check/Controller/ConnectivityTestController.cs
+++ ase-dependency-check/Controller/ConnectivityTestController.cs
@@ -1,17 +1,18 @@
 ﻿using ASEDependencyCheck.Model;
 using ASEDependencyCheck.Services.Interfaces;
 using CommandDotNet;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Principal;
 using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ASEDependencyCheck.Controller
 {
-
     public class ConnectivityTestController
     {
         private readonly ISocketConnection _conn;
@@ -28,8 +29,9 @@ namespace ASEDependencyCheck.Controller
 
         }
 
-        private async Task<bool> LoadJsonFile()
+        private async Task<bool> LoadJsonFile(string endptfilename)
         {
+            
             var options = new JsonSerializerOptions
             {
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
@@ -37,7 +39,7 @@ namespace ASEDependencyCheck.Controller
             };
             try
             {
-                string jsonString = await File.ReadAllTextAsync("endpoints.json");
+                string jsonString = await File.ReadAllTextAsync(endptfilename);
                 _endpoints = JsonSerializer.Deserialize<Endpoints>(jsonString, options);
 
                 return true;
@@ -48,56 +50,119 @@ namespace ASEDependencyCheck.Controller
                 _logger.LogError("Unable to find endpoints.json file");
                 return false;
             }
-            
+        }
 
-            
+        private async Task<DependencyCheckSummary> CheckDependency(List<string> dependencies)
+        {
+            string hostName;
+            int port, i=1, maxTries = 4;
+            string[] temp;
+
+            DependencyCheckSummary dependencySummary = new DependencyCheckSummary();
+            // ds.totalDependenciesCalled = _endpoints.dependencies.Count;
+            dependencySummary.totalDependenciesCalled = dependencies.Count;
+            dependencySummary.totalDependenciesFailed = 0;
+            dependencySummary.endpointsFailed = new List<string>();
+
+            foreach (string dependency in dependencies)
+            {
+                temp = dependency.Split(":");
+                if (temp.Length == 1)
+                {
+                    hostName = temp[0];
+                    port = 80;
+                }
+                else
+                {
+                    hostName = temp[0];
+                    port = Convert.ToInt32(temp[1]);
+                }
+                _logger.LogInformation($"Testing connectivity to endpoint {i}/{dependencies.Count} - {hostName}:{port} for {maxTries} times");
+              //  await _conn.Tcpping(hostName, port, maxTries);
+
+                TcppingErrorSummary tcpsummary = await _conn.Tcpping(hostName, port, maxTries);
+                _logger.LogInformation("Returned from tcpping");
+                if (tcpsummary.successRate != 100)
+                {
+                    dependencySummary.totalDependenciesFailed++;
+                    _logger.LogInformation("Dependencies failed so far:"+ dependencySummary.totalDependenciesFailed.ToString());
+                     dependencySummary.endpointsFailed.Add(tcpsummary.hostName + ":" + tcpsummary.port.ToString());
+                }
+                i++;
+
+                _logger.LogInformation("");
+                _logger.LogInformation("");
+            }
+            dependencySummary.nextSteps = "Next Steps: Unblock connection to these failed dependencies. Check NSG/Firewall/Network Virtual Appliance configuration to identify & remove blocking rules";
+            return dependencySummary;
+        }
+
+        //private async Task PrintSummary(DependencyCheckSummary dependencySummary, string platformType)
+        private void PrintSummary(DependencyCheckSummary dependencySummary, string platformType)
+        {
+                _logger.LogInformation("");
+                _logger.LogInformation("************ASE Dependency Check Summary for " + platformType + "************");
+                _logger.LogInformation("Total number of " + platformType + " Endpoints tested = " + dependencySummary.totalDependenciesCalled);
+                _logger.LogInformation("Total number of " + platformType + " Endpoints failed = " + dependencySummary.totalDependenciesFailed);
+            if (dependencySummary.totalDependenciesFailed != 0)
+            {
+                _logger.LogInformation("Connections to below " + platformType + " dependencies failed at least 1 out of 4 tries");
+                _logger.LogInformation("");
+                foreach (string endpointfailed in dependencySummary.endpointsFailed)
+                {
+                    _logger.LogInformation(endpointfailed);
+                }
+                _logger.LogInformation("");
+                _logger.LogInformation(dependencySummary.nextSteps);
+            }
         }
 
         [Command(Name = "test-connectivity",
-        Usage = "get-token -e endpoints.json",
-        Description = "Gets a token for the specified resource")]
+        Usage = "test-connectivity -e endpoints.json -p platformType",
+        Description = "Tests connectivity to all endpoints in endpoints.json based on platformType")]
         public async Task TestConnectivity(
         [Option(LongName = "endpoint", ShortName = "e",
         Description = "location of endpoint.json file which contains the list of endpoints to tcpping ")]
-        string endpointFileName)
+        string endpointFileName, [Option(LongName = "platformType", ShortName = "p",
+        Description = "Indicates the platform type (windows/linux) to target dependency checks")] //Sample test command: ASEDependencyCheck.exe test-connectivity -e testendpoints.json -p windows
+        string platformType)
         {
-            string hostName; int port, maxTries = 4;
-            // 1. Validate the endpoint
+            // 1. Validate the endpoint filename
             if (endpointFileName.Contains(".json"))
             {
-                bool fileLoadSuccessful = await LoadJsonFile();
+                
+                bool fileLoadSuccessful = await LoadJsonFile(endpointFileName);
                 if(fileLoadSuccessful)
                 {
-                    int i = 1;
                     _logger.LogInformation("Loaded Json file with Endpoints.");
                     _logger.LogInformation("");
-                    foreach (string dependency in _endpoints.dependencies)
+
+                    if (platformType == "all")
                     {
-                        string[] temp =  dependency.Split(":");
-                        if(temp.Length == 1)
-                        {
-                            hostName = temp[0];
-                            port = 80;
-                        }
-                        else
-                        {
-                            hostName = temp[0];
-                            port = Convert.ToInt32(temp[1]);
-                        }
-                        _logger.LogInformation($"Testing connectivity to endpoint {i}/{_endpoints.dependencies.Count} - {hostName}:{port} for {maxTries} times");
-                        await _conn.Tcpping(hostName, port, maxTries);
-                        i++;
+                        DependencyCheckSummary dependencysummarywindows = await CheckDependency(_endpoints.dependencies["windowsdependencies"]);
+                        DependencyCheckSummary dependencysummarylinux = await CheckDependency(_endpoints.dependencies["linuxdependencies"]);
 
-                        _logger.LogInformation("");
-                        _logger.LogInformation("");
+                        _logger.LogInformation("-----------------------------------------------------------------------------------------------------------");
+                        PrintSummary(dependencysummarywindows, "windows");
+                        PrintSummary(dependencysummarylinux, "linux"); 
                     }
-
+                    else if (platformType == "linux" || platformType == "windows")
+                    {
+                        DependencyCheckSummary dependencysummary = await CheckDependency(_endpoints.dependencies[$"{platformType}dependencies"]);
+                        _logger.LogInformation("-----------------------------------------------------------------------------------------------------------");
+                        PrintSummary(dependencysummary, platformType);
+                    }
+                    else
+                    {
+                        _logger.LogError("Invalid platformType value. Allowed values 'windows' or 'linux' or 'all'");
+                    }
                 }
             }
             else
             {
                 _logger.LogError("Invalid File extenstion entered. Please provide json as input");
             }
+            _logger.LogInformation("-----------------------------------------------------------------------------------------------------------");
         }
     }
 }
